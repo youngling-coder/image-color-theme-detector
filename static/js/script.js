906849dacf4b2cfca34c1ebf85e53c3f3ce6a79c
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-18 22:50:32 +0200

    add visual styles for copying color to the clipboard

--- static/js/script.js
+++ static/js/script.js
@@ -58,6 +58,7 @@ uploadButton.addEventListener('click', async () => {
         // Get host url
         const baseUrl = window.location.origin;
 
+        // Fetch data from the server
         const response = await fetch(`${baseUrl}/upload_image`, {
             method: 'POST',
             body: formData
@@ -68,8 +69,10 @@ uploadButton.addEventListener('click', async () => {
             
         if (response.ok) {
             
+            // Reset main colors
             colorDisplay.innerHTML = "";
             
+            // Add new main colors
             for (var i = 0; i < 5; i++) {
                 let color = resultColors[i]["hex_"];
                 
@@ -78,13 +81,11 @@ uploadButton.addEventListener('click', async () => {
                 color_element.innerHTML = `<h4 style="color: ${color};" class="color-name">${color}</h4>`;
                 color_element.className = "color-box";
                 color_element.style.backgroundColor = color;
+
+                // Add color to the HTML code
                 colorDisplay.appendChild(color_element);
             }
-            // background.style.background = `linear-gradient(135deg, ${resultColors[0]["hex_"]}, ${resultColors[1]["hex_"]}, ${resultColors[2]["hex_"]}, ${resultColors[3]["hex_"]}, ${resultColors[4]["hex_"]})`;
-
-            const colorBoxes = document.querySelectorAll('.color-box');
-
-
+            
             // Function converts rgb(a, b, c) color string to #123456 format
             function rgbToHex(rgb) {
                 const rgbValues = rgb.match(/\d+/g);
@@ -93,27 +94,55 @@ uploadButton.addEventListener('click', async () => {
                 const b = parseInt(rgbValues[2]).toString(16).padStart(2, '0');
                 return `#${r}${g}${b}`;
             }
+        
+            // Get colornames of the color boxes
+            const colorNames = document.querySelectorAll('.color-name');
+            
+            const changeText = (name, newText) => {
 
-            colorBoxes.forEach(box => {
-                // Get the background color of the clicked element
-                const bgColor = window.getComputedStyle(box).backgroundColor;
+                // Change text of the color box colorname
+                name.classList.remove("fade-in");
+                name.classList.add("fade-out");
+                
+                setTimeout(() => {
+                    name.innerHTML = `${newText}`;
+                    name.classList.remove("fade-out");
+                    name.classList.add("fade-in");
+                }, 200);
+            };
 
-                // Convert the RGB color to hex
-                const hexColor = rgbToHex(bgColor).toLocaleUpperCase();
 
-                box.addEventListener('click', function () {
+            colorNames.forEach(name => {
+
+                // Get HEX color representation of the color box
+                const color = name.innerHTML;
+
+                // Add event listener when mouse leaves the element
+                name.addEventListener('mouseout', function () {
+                    changeText(name, color);
+                });
+
+                // Add event listener when mouse enters the element
+                name.addEventListener('mouseover', function () {
+                    changeText(name, "Copy");
+                });
+
+                // Add event listener when user clicks the element
+                name.addEventListener('click', function () {
 
                     // Copy the hex color to clipboard using hidden textarea
-                    clipboardTextarea.value = hexColor;
+                    clipboardTextarea.value = color;
                     clipboardTextarea.select();
                     document.execCommand('copy');
+                    changeText(name, "Copied!");
                 });
             });
+
         } else {
             if (response.status == 415) {
                 alert(resultColors.detail);
             } else {
-                alert('Unknown error! File upload failed!');
+                alert(`Unknown error! File upload failed! Error code: ${response.status}`);
             }
         }
         
